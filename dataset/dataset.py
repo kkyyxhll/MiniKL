7cece800893780713b292ea9bebb6df158959bb4
kkyyxhll <2243036632@qq.com>
2025-06-01 03:15:21 +0800

    add tqdm

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -5,7 +5,7 @@ import torch
 from torch.utils.data import Dataset
 
 import time
-
+from tqdm import tqdm
 class PretrainDataset(Dataset):
     def __init__(self,tokenizer, data_jsonl_path):
         self.tokenizer = tokenizer
@@ -16,16 +16,17 @@ class PretrainDataset(Dataset):
         start_time = time.time()
         dataset = []
         with open(data_jsonl_path, "r", encoding="utf-8") as f:
-            for line in f:
-                content = json.loads(line)["content"]
-                if type(content) != list:
-                    #[]
-                    # dict{"input_ids":list(tuple...), "padding_masks":list(tuple...)}
-                    chunks= self.tokenizer.pretrain_tokenize(content)
+            with tqdm(f, desc="处理数据行", unit="行") as pbar:
+                for line in pbar:
+                    content = json.loads(line)["content"]
+                    if type(content) != list:
+                        #[]
+                        # dict{"input_ids":list(tuple...), "padding_masks":list(tuple...)}
+                        chunks= self.tokenizer.pretrain_tokenize(content)
 
-                for i in range(len(chunks["input_ids"])):
-                    temp_dict = {"input_ids": chunks["input_ids"][i], "padding_masks": chunks["padding_masks"][i]}
-                    dataset.append(temp_dict)
+                    for i in range(len(chunks["input_ids"])):
+                        temp_dict = {"input_ids": chunks["input_ids"][i], "padding_masks": chunks["padding_masks"][i]}
+                        dataset.append(temp_dict)
 
         print(f"数据集载入完成,time:{time.time()-start_time}")
         return dataset
@@ -67,36 +68,37 @@ class SFTDataset(Dataset):
         start_time = time.time()
         tokens_masks_list = []
         with open(data_jsonl_path, "r", encoding="utf-8") as f:
-            for line in f :
-                conversations = json.loads(line)["conversations"]
-                prompt = ""
-                for conversation in conversations:
-                    role = conversation["role"]
-                    content = conversation["content"]
-                    prompt += f"<|{role}|>{content}</s>\n"
-                tokens = self.tokenizer.tokenize(prompt)[0]
-                if len(tokens) >= self.tokenizer.max_seq_len:
-                    continue
-                masks = []
-                flag_assistant = 0
-                assistant_start = self.tokenizer.vocab_dict["<|assistant|>"]
-                pad = self.tokenizer.vocab_dict["<pad>"]
-                end = self.tokenizer.vocab_dict["</s>"]
-                for token in tokens:
-                    if flag_assistant == 0:
+            with tqdm(f, desc="处理数据行", unit="行") as pbar:
+                for line in pbar:
+                    conversations = json.loads(line)["conversations"]
+                    prompt = ""
+                    for conversation in conversations:
+                        role = conversation["role"]
+                        content = conversation["content"]
+                        prompt += f"<|{role}|>{content}</s>\n"
+                    tokens = self.tokenizer.tokenize(prompt)[0]
+                    if len(tokens) >= self.tokenizer.max_seq_len:
+                        continue
+                    masks = []
+                    flag_assistant = 0
+                    assistant_start = self.tokenizer.vocab_dict["<|assistant|>"]
+                    pad = self.tokenizer.vocab_dict["<pad>"]
+                    end = self.tokenizer.vocab_dict["</s>"]
+                    for token in tokens:
+                        if flag_assistant == 0:
+                            masks.append(0)
+                        if flag_assistant == 1:
+                            masks.append(1)
+                            if token == end:
+                                flag_assistant = 0
+                        if token == assistant_start:
+                            flag_assistant = 1
+
+                    while len(tokens) < self.tokenizer.max_seq_len:
+                        tokens.append(pad)
                         masks.append(0)
-                    if flag_assistant == 1:
-                        masks.append(1)
-                        if token == end:
-                            flag_assistant = 0
-                    if token == assistant_start:
-                        flag_assistant = 1
 
-                while len(tokens) < self.tokenizer.max_seq_len:
-                    tokens.append(pad)
-                    masks.append(0)
-
-                tokens_masks_list.append((tokens, masks))
+                    tokens_masks_list.append((tokens, masks))
         print(f"SFTDataset had loaded, spend_time:{time.time()-start_time:.4f}")
         return tokens_masks_list
     def __getitem__(self, idx):
@@ -105,6 +107,17 @@ class SFTDataset(Dataset):
         return tokens[0:-1], tokens[1:], masks[1:]
     def __len__(self):
         return len(self.tokens_masks_list)
+
+class DPODataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+    def __len__(self):
+        pass 
+
 
 if __name__ == "__main__":
 
